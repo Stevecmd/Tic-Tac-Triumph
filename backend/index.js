a9a8a6a995a52e3cba785ba81d7660281fc14957
Restyled.io <commits@restyled.io>
2024-08-25 18:49:17 +0000

    Restyled by prettier

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const {createServer} = require('http');
-const {Server} = require('socket.io');
-const path = require('path');
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -9,17 +9,21 @@ const port = 3000;
 const server = createServer(app);
 const io = new Server(server);
 
-app.get(
-    '/',
-    (req,
-     res) => { res.sendFile(path.join(__dirname, '../frontend/index.html')); });
-
-io.on('connection', (socket) => {
-  console.log('player connected');
-
-  socket.on('test', (msg) => { console.log(msg); });
-  socket.on('disconnect', () => { console.log('player disconnected'); });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-server.listen(
-    port, () => { console.log(`app listening at http://localhost:${port}`); });
+io.on("connection", (socket) => {
+  console.log("player connected");
+
+  socket.on("test", (msg) => {
+    console.log(msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("player disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
