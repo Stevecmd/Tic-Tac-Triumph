b26ded6685fa5db65bfbe859caa3221aee227e61
ugoMusk <ugobothankgod@gmail.com>
2024-08-28 23:30:33 +0100

    updates from master

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ const cors = require("cors");
 
 /* helpers */
 const Lobby = require('./lobby');
-const express = require('express');
-const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -64,14 +62,6 @@ io.on('connection', (socket) => {
     });
 });
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// Route to serve index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
 });
